d0b0440a22f3423f184fb0ee542de8ff87644921
Salvador <salbecerra1023@gmail.com>
2019-05-12 18:46:59 -0700

    flask updated

--- Code/app.py
+++ Code/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 import random
 from sample import sample
 from dictionary_words import get_words, random_word_sentence
-from Markov import MarkovChain
+from higher_markov import MarkovChain
 
 app = Flask(__name__)
 
@@ -10,5 +10,5 @@ app = Flask(__name__)
 def tweet():
     word_list = get_words('1984.txt')
     markov_chain = MarkovChain(word_list)
-    sentence = markov_chain.chain_traversal(10)
+    sentence = markov_chain.chain_traversal(30)
     return render_template("index.html", sentence = sentence)
